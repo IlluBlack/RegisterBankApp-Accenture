c24ccc06ce73670f468d24623b589baa39c61f47
MichelleBohórquez <michii_juana@hotmail.com>
2019-09-21 00:16:15 -0500

    Project with Angular Material and Google Fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesignModule } from 'src/app/app-material-design/material-design.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
